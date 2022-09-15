b4394178c02f69927ff8c88716c556c33a81ca49
Jakeliny Gracielly <jakeliny.dev@gmail.com>
2022-09-15 17:01:43 -0300

    check position and if contain letter in word

--- assets/main.js
+++ assets/main.js
@@ -15,14 +15,39 @@ for (i = 0; i < wordDay.length; i++) {
   letters.push(word.appendChild(document.createElement('input')));
 }
 
-btnSubmit.addEventListener('click', () => {
+function checkWord() {
   let wordUser = '';
+
   for (i = 0; i < letters.length; i++) {
     wordUser += letters[i].value.toUpperCase();
   }
+
   if (wordUser === wordDay) {
     alert('Correct!');
-  } else {
-    alert('Incorrect!');
+    return true
+  }
+  return false
+}
+
+function checkLettersPosition() {
+  for (i = 0; i < letters.length; i++) {
+    if (letters[i].value.toUpperCase() === wordDay[i]) {
+      letters[i].classList.add('correctPosition');
+    }
+  }
+}
+
+function checkContainLetters() {
+  for (i = 0; i < letters.length; i++) {
+    if (wordDay.includes(letters[i].value.toUpperCase())) {
+      letters[i].classList.add('corretLetter');
+    }
+  }
+}
+
+btnSubmit.addEventListener('click', () => {
+  if (!checkWord()) {
+    checkContainLetters();
+    checkLettersPosition();
   }
 });
