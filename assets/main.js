3ad8a31e525d8043f3ab1c76d514320bb2fbd04c
Jakeliny Gracielly <jakeliny.dev@gmail.com>
2022-09-15 17:36:16 -0300

    add new lines

--- assets/main.js
+++ assets/main.js
@@ -1,53 +1,78 @@
 const words = document.querySelector('#box-words');
 const btnSubmit = document.querySelector('#btn-submit');
+const btnNewLine = document.querySelector('#btn-new-line');
 
 const wordDay = 'WORLD';
 const allLettersString = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const allLetters = allLettersString.split('');
 
-
+const pastLetters = [];
 const lettersNow = [];
+let word;
 
-const word = words.appendChild(document.createElement('div'));
-word.classList.add('word');
+function newLine() {
+  if (lettersNow.length != 0) {
+    disabledLetters();
+  }
+  word = words.appendChild(document.createElement('div'));
+  word.classList.add('word');
+  for (i = 0; i < wordDay.length; i++) {
+    lettersNow.push(word.appendChild(document.createElement('input')));
+  }
+}
 
-for (i = 0; i < wordDay.length; i++) {
-  lettersNow.push(word.appendChild(document.createElement('input')));
+function disabledLetters() {
+  lettersNow.map(letter => {
+    letter.setAttribute("disabled", "disabled");
+  })
+  pastLetters.push(lettersNow);
+  word.classList.add('past-word');
 }
 
 function checkWord() {
   let wordUser = '';
 
-  for (i = 0; i < lettersNow.length; i++) {
-    wordUser += lettersNow[i].value.toUpperCase();
-  }
+  lettersNow.map(letter => {
+    wordUser += letter.value.toUpperCase();
+  });
 
   if (wordUser === wordDay) {
-    alert('Correct!');
     return true
   }
   return false
 }
 
 function checkLettersPosition() {
-  for (i = 0; i < lettersNow.length; i++) {
-    if (lettersNow[i].value.toUpperCase() === wordDay[i]) {
-      lettersNow[i].classList.add('correctPosition');
+  lettersNow.map(letter => {
+    if (letter.value.toUpperCase() === wordDay[lettersNow.indexOf(letter)]) {
+      letter.classList.add('corretLetterPosition');
     }
-  }
+  });
 }
 
 function checkContainLetters() {
-  for (i = 0; i < lettersNow.length; i++) {
-    if (wordDay.includes(lettersNow[i].value.toUpperCase())) {
-      lettersNow[i].classList.add('corretLetter');
+  lettersNow.map(letter => {
+    if (wordDay.includes(letter.value.toUpperCase())) {
+      letter.classList.add('corretLetter');
     }
-  }
+  });
+}
+
+function youWin() {
+  disabledLetters();
+  alert('You Win!');
 }
 
 btnSubmit.addEventListener('click', () => {
   if (!checkWord()) {
     checkContainLetters();
+    checkLettersPosition();
+    newLine();
+    return
   }
-  checkLettersPosition();
+  alert('You win!');
+  youWin();
+
 });
+
+window.document.onload = newLine();
